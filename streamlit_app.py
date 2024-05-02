af865cf33d62783cc1dcd5b50c415a2eee4c8599
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-05-02 12:50:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
 
-foto = st.camera_input("Foto")
-st.divider()
-if foto is not None:
-  st.image(foto)
+# el objeto es de tipo fichero, no imagen
+img_file_buffer = st.camera_input("Haz una foto")
+
+if img_file_buffer is not None:
+    # convertimos a imagen
+    img = Image.open(img_file_buffer)
+
+    # convertimos a array
+    img_array = np.array(img)
+
+    # Check the type of img_array:
+    # Should output: <class 'numpy.ndarray'>
+    #st.write(type(img_array))
+
+    # muestra altura, anchura y canales    
+    st.write(img_array.shape)
